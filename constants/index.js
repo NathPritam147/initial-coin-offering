247bf54de7c0d472635ef1045b0eafff1b90d4ab
Pritam Nath <pnath147@gmail.com>
2022-12-02 09:28:39 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the Crypto Spartans Contract that you deployed
+const CRYPTO_SPARTANS_CONTRACT_ADDRESS = "0x760D81E325c976DdD45ffF8bBC598f1265b8092d";
+
+module.exports = { CRYPTO_SPARTANS_CONTRACT_ADDRESS,  };
